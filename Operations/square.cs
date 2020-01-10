715ba8ddcac7d332887d432eb31f4ab32512abff
MwangoS <natchaziya@gmail.com>
2020-01-10 11:19:31 +0200

    Add project files.

---
+++ Operations/square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class Square
+    {
+        //public static int Numb(int Numb)
+        //{
+          //  Console.WriteLine("Enter a Number : ");
+          //  int Number = Convert.ToInt16(Console.ReadLine());
+          //  double Sqr = Math.Pow(Number, 2);
+           // Console.WriteLine("Square of {0} is: {1}", Number, Sqr);
+          //  Console.ReadLine();
+       // }
+
+    }
+        
+}
