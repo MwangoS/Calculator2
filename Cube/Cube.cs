f988d6f61b47a219cbc3dd04e4d618f4c258b285
MwangoS <natchaziya@gmail.com>
2020-01-17 13:08:40 +0200

    Feature: Added a new calculator operation

---
+++ Cube/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Operations;
+
+namespace CubeNumber
+{
+    public interface ICube
+    {
+        public dynamic CubeNumber(dynamic c);
+    }
+    public class Cubed
+    {
+        public dynamic result;
+        public dynamic CubeNumber(dynamic c)
+        {
+            result = CubeNumber(c);
+            return result;
+        }
+    }
+}
